25a804762d2f872f9fee0bacbe27e52c0305c890
연구개발전담부 <wdpb@mig.kr>
2025-10-31 18:34:34 +0900

    design: 블로그연동

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,14 +2,22 @@ import Contact from "@/components/Contact";
 import GoogleMap from "@/components/GoogleMap";
 import Hero from "@/components/Hero";
 import React from "react";
+import Image from "next/image";
 
 export default function Map() {
   return (
     <>
-      <section className=" mb-40">
-        <Hero title={"오시는 길"} subtitle={"LK 관세사무소 위치를 안내드립니다."} img={"/img/bg_work_solutions04.jpg"} />
-
-        <GoogleMap />
+      <section className="mb-40">
+        <Hero title={"자료실"} subtitle={"LK 관세사무소 블로그에서 자료들을 확인해보실 수 있습니다"} img={"/img/bg_work_solutions04.jpg"} />
+      </section>
+      <section className="flex flex-col justify-center items-center mb-40 gap-4">
+        <h2>관련 자료를 확인해 보세요</h2>
+        <a href="https://blog.naver.com/lek9740">
+          <div className="flex flex-col justify-center items-center gap-4">
+            <Image src={"/img/blog.webp"} width={100} height={100} alt={"블로그아이콘"} className="rounded-md" priority />
+            <p className="text-sm  text-green-600/80">아이콘을 선택하면 이동합니다.</p>
+          </div>
+        </a>
       </section>
 
       <section>
