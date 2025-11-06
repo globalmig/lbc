cd114d2054903aec3a9e1c247efec932a35d4ad5
연구개발전담부 <wdpb@mig.kr>
2025-11-06 11:19:20 +0900

    fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ interface HeroProps {
 
 export default function Hero(item: HeroProps) {
   return (
-    <div className="relative h-80 md:h-[640px] flex flex-col justify-center bg-gray-200">
+    <div className="relative h-80 md:h-[640px] flex flex-col justify-center ">
       <div className="absolute w-full h-full overflow-hidden">
         <Image src={item.img} alt="Hero Image" fill priority={item.priority} sizes="100vw" quality={85} className="blur-sm h-full object-cover object-center " />
       </div>
